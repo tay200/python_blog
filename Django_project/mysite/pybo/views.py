31b72d423cad234137c64b8d6fd9b993b19fcaa9
Jinseok <jinseok5646@gmail.com>
2020-10-22 17:20:47 +0900

    03_02

--- Django_project/mysite/pybo/views.py
+++ Django_project/mysite/pybo/views.py
@@ -2,11 +2,23 @@ from .models import Question
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 def index(request):
     # pybo 목록 출력
+    # ---------------------------------------- [edit] ---------------------------------------- #
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list' : question_list}
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
+    # ---------------------------------------------------------------------------------------- #
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -22,7 +34,6 @@ def answer_create(request, question_id):
     pybo 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    # ---------------------------------------- [edit] ---------------------------------------- #
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
@@ -35,13 +46,11 @@ def answer_create(request, question_id):
         form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
-    # ---------------------------------------------------------------------------------------- #
 
 def question_create(request):
     """
     pybo 질문등록
     """
-    # ---------------------------------------- [edit] ---------------------------------------- #
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
@@ -53,4 +62,3 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
-    # ---------------------------------------------------------------------------------------- #
